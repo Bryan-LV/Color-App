7f9dd0d6d6968a82d2cfe517192bb3883e850f89
Bryan Almaraz <bryan.almaraz96@gmail.com>
2019-12-06 09:01:18 +0100

    create react app,  created color pallete

---
+++ src/Pallete.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Pallete(props) {
+  return (
+    <div className="pallete">
+      {/* Navbar goes here */}
+      <div>Pallete</div>
+      {/* Footer goes here */}
+    </div>
+  )
+}

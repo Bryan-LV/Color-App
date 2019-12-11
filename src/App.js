4761666e69d4daaaa5ffb95238a2f897b4c2bdff
Bryan Almaraz <bryan.almaraz96@gmail.com>
2019-12-11 19:56:30 +0100

    color converter

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import React from 'react';
 import './App.css';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
 function App() {
-
-  const colorCheck = () => {
-    console.log('color check function');
-    console.log(...seedColors);
-  }
+  console.log(generatePalette(seedColors[4]));
   return (
     <>
     <Palette palette={seedColors[4]}/>
-    {colorCheck()}
     </>
   );
 }
--- src/Colorbox.js
+++ src/Colorbox.js
@@ -1,14 +1,30 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 import './colorbox.css'
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
-export default class Colorbox extends Component {
-  render() {
-    return (
-      <div className="colorbox" style={{backgroundColor: this.props.background}}>
+export default function Colorbox(props) {
+  const [copy, setCopy] = useState(false);
+
+  const handleCopy = () => {
+    setCopy(true);
+    setTimeout(() => setCopy(false), 1200);
+  }
+
+  const showOverlay = copy ? 'show' : '';
+
+  return (
+    <CopyToClipboard text={'hello'} onCopy={handleCopy}>
+      <div className="colorbox" style={{backgroundColor: props.background}}>
+        <div style={{backgroundColor:props.background}} className={`copy-overlay ${showOverlay}`}></div>
+        <div className={`copy-msg ${showOverlay}`}>
+          <h1>copied!</h1>
+          <p>{props.background}</p>
+        </div>
         <p className="see-more">More</p>
         <button className="copy-btn">Copy</button>
-        <p className="color-name">{this.props.name}</p>
+        <p className="color-name">{props.name}</p>
       </div>
-    )
-  }
+    </CopyToClipboard>
+  )
 }
+
